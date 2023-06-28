83e01ec1d1f3c01f61a5e766a59a77a42dd46625
Pranjali Mishra <101552456+pranjalimishra21@users.noreply.github.com>
2023-06-29 01:45:18 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,22 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Landing Page",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Landing Page-Pizaria best piza around the world.",
+    url: "https://pranjalimishra21.github.io/OIBGRIP/"",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Portfolio",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Simple and easy Portfolio Desiging",
+    url: "https://github.com/pranjalimishra21/OIBGRIP-TASK-2-",
   },
   {
-    title: "My Resume Site",
+    title: "",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "",
+    url: "",
   },
   {
     title: "GitHub Codespaces and github.dev",
