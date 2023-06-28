b7f3c4ebef4caa1e19d9ea3000758c515b3dbe3d
Pranjali Mishra <101552456+pranjalimishra21@users.noreply.github.com>
2023-06-29 01:36:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pranjali Mishra",
+  title: "Web Development & Desiging",
+  email: "pranjalimishra2103@gmail.com",
+  gitHub: "pranjalimishra21",
+  instagram: "_pranjali_mishra_21",
+  linkedIn: "Pranjali Mishra",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
