881ccdad32a81953ac600ab8d3b94718d4fa5e41
Pranjali Mishra <101552456+pranjalimishra21@users.noreply.github.com>
2023-06-29 01:40:58 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a MCA Student studying at International Institute of Professional Studies. I enjoy creating unique and simplistic websites as my keen interest is in web develpoment.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Web Development & Designing experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
